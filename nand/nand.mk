5579220c456dd42cdec8dd8e67ce6c72a689ade2
Anthony Sepa <anthonysepa@yahoo.ca>
2017-04-12 18:53:14 -0300

    Remove kernel versioning from rknand module name

--- nand/nand.mk
+++ nand/nand.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
     vendor/rockchip/common/nand/modules/arm/drmboot.ko:root/drmboot.ko
 else
 PRODUCT_COPY_FILES += \
-    vendor/rockchip/common/nand/modules/arm/rk30xxnand_ko.ko.3.10.0:root/rk30xxnand_ko.ko.3.10.0 \
+    vendor/rockchip/common/nand/modules/arm/rk30xxnand_ko.ko.3.10.0:root/rk30xxnand_ko.ko \
     vendor/rockchip/common/nand/modules/arm/drmboot.ko:root/drmboot.ko
 endif
 endif
